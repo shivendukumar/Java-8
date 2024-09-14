b09b76a2d3cfbe6f4ab9e3b733e902a541b58a1e
shivendukumar <bushivendu@gmail.com>
2024-09-14 10:27:48 +0530

    Java8 first commit

--- Java_8/src/com/java8/stream/Lab01.java
+++ Java_8/src/com/java8/stream/Lab01.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lab01 {
-
 	public static void main(String[] args) {
-		
 		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
 		System.out.println(list);
 		System.out.println("=============================");
