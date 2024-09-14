f196ab623f9876d8599c5840acd5c648d5dad3bd
shivendukumar <bushivendu@gmail.com>
2024-09-14 12:22:28 +0530

    stream operation with skip and limit functionality

--- Java_8/src/com/java8/stream/Lab06.java
+++ Java_8/src/com/java8/stream/Lab06.java
@@ -3,6 +3,14 @@ package com.java8.stream;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+ * Author Shivendu Kumar
+ * skip is used to skip a certain number of elements at the beginning of the
+ * stream, allowing you to start processing from a specified position further
+ * along in the stream.
+ */
+
+
 public class Lab06 {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
@@ -13,10 +21,6 @@ public class Lab06 {
 		list.stream()
 			.skip(6).limit(8)
 			.forEach(System.out::println);
-		/*
-		 * skip is used to skip a certain number of elements at the beginning of the
-		 * stream, allowing you to start processing from a specified position further
-		 * along in the stream.
-		 */
+		
 	}
 }
